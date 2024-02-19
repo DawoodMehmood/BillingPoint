42fd4512f2edcb5845d9c66d987f5f070347ec16
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-19 23:40:22 +0500

    scroll to top button component

---
+++ frontend/src/components/scrollToTopButton.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Function to detect scroll position
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    isVisible && (
+      <FontAwesomeIcon
+        icon={faCircleArrowUp}
+        className="fixed bottom-5 right-5 z-50 cursor-pointer text-4xl text-magenta"
+        onClick={scrollToTop}
+        title="Scroll To Top"
+      />
+    )
+  );
+};
+
+export default ScrollToTopButton;

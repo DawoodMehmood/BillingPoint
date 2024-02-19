f2e4259a88f89b91838e46aa144a7815abccadf3
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-19 23:35:46 +0500

    tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "celestial-blue": "#058ED9",
+        seagreen: "#4BC6B9",
+        magenta: "#B57BA6",
+      },
+    },
+  },
+  plugins: [],
+};

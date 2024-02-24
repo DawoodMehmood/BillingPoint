ceb969375c406fc085828e95d3156abcf1b8d241
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-25 02:19:32 +0500

    Index js frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "./context/authContext";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <ToastContainer position="top-center" autoClose={2000} />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);

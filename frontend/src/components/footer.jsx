be3cd57dc35c359c085c1e09b80ed37f62408520
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-02 22:25:05 +0500

    Make footer component

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center gap-2 w-full bg-beige fixed h-10 bottom-0">
+      <p>&copy;</p>
+      <i>BillingHub - 2024</i>
+    </div>
+  );
+};
+
+export default Footer;
